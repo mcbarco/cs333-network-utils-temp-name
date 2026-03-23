cb383ec4fdcde370102c863cf05aadc112ef3141
Megan Barco <meganbarco@u.boisestate.edu>
2026-03-23 11:44:40 -0600

    basic packet class

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class Packet:
+    # TODO: Implement the Packet class with appropriate attributes and methods
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return f"Packet(data={self.data})"
